96b2d4485f3c44d874d426ac3aab9d39dec1f91f
MarcelN911 <marceln911@icloud.com>
2026-01-28 14:03:26 -0500

    Initial commit: Add HTML structure, CSS styles, and JavaScript files for Kühlschrank-LeerAPP with menu items and details.

---
+++ scripts/database.js
@@ -0,0 +1,265 @@
+let menu = {
+    "Burger": {
+        "banner": 
+            {
+                "banner-img": "./assets/img/Burger.png",
+            },  
+        "info": [
+            {
+            "name": "Signature Cheeseburger",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Brioche-Bun", "Rindfleisch-Patty", "Cheddar", "Gurken", "Ketchup", "Senf", "Zwiebeln", "Salat"],
+            "price": 10.99,
+            "img": "./assets/img/Burger1.jpeg",
+            },
+            {
+            "name": "Crispy Chicken Burger",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Sesam-Brötchen", "panierte Hähnchenbrust", "Eisbergsalat", "Mayonnaise", "Tomaten"],
+            "price": 9.99,
+            "img": "./assets/img/Burger2.jpeg",
+            },
+            {
+            "name": "American Classic Burger",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Weizen-Brötchen", "Rindfleisch-Patty", "Tomatenscheiben", "Zwiebelringe", "Gewürzgurken", "Salat", "Barbecuesauce"],
+            "price": 12.99,
+            "img": "./assets/img/Burger3.jpeg",
+            },
+            {
+            "name": "Smashed Burger",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Potato-Roll", "flach gepresstes Rindfleisch-Patty", "doppelt Cheddar", "karamellisierte Zwiebeln", "Senf-Mayo-Sauce"],
+            "price": 11.99,
+            "img": "./assets/img/Burger4.jpeg",
+            },
+            {
+            "name": "BBQ Bacon Burger",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Weizen-Brötchen", "Rindfleisch-Patty", "Cheddar", "knuspriger Bacon", "Röstzwiebeln", "Barbecuesauce", "Salat"],
+            "price": 13.49,
+            "img": "./assets/img/Burger5.jpeg",
+            },
+        ],
+    },
+
+    "Pizza (30cm)": {
+        "banner": 
+            {
+                "banner-img": "./assets/img/Pizza.png",
+            },
+        "info": [
+            {
+            "name": "Margherita",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Tomatensauce", "Mozzarella", "frisches Basilikum", "Olivenöl"],
+            "price": 8.99,
+            "img": "./assets/img/Pizza1.jpeg",
+            },
+            {
+            "name": "Schnitzel",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Sauce-Hollondaise", "Mozzarella", "Schnitzel-Streifen", "Brokolli"],
+            "price": 12.99,
+            "img": "./assets/img/Pizza2.jpeg",
+            },
+            {
+            "name": "Prosciutto",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Sauce-Hollondaise", "Mozzarella", "Parmaschinken", "Parmesan"],
+            "price": 13.99,
+            "img": "./assets/img/Pizza3.jpeg",
+            },
+            {
+            "name": "Tonno",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Tomatensauce", "Mozzarella", "Thunfisch", "rote Zwiebeln", "Oliven"],
+            "price": 11.99,
+            "img": "./assets/img/Pizza4.jpeg",
+            },
+            {
+            "name": "Salami",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Tomatensauce", "Mozzarella", "Salami"],
+            "price": 10.99,
+            "img": "./assets/img/Pizza5.jpeg",
+            },
+        ],
+    },
+    
+    "Pasta": {
+        "banner": 
+            {
+                "banner-img": "./assets/img/Burger.png",
+            },
+        "info": [
+            {
+            "name": "Spaghetti Bolognese",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Spaghetti", "Rinderhackfleisch", "Tomatensauce", "Parmesan", "frisches Basilikum"],
+            "price": 9.99,
+            "img": "./assets/img/Pasta1.jpeg",
+            },
+            {
+            "name": "Fettuccine Alfredo",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Fettuccine", "Sahnesauce", "Parmesan", "Petersilie"],
+            "price": 10.99,
+            "img": "./assets/img/Pasta2.jpeg",
+            },
+            {
+            "name": "Penne Arrabbiata",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Penne", "würzige Tomatensauce", "Knoblauch", "Chiliflocken", "Basilikum"],
+            "price": 8.99,
+            "img": "./assets/img/Pasta3.jpeg",
+            },
+            {
+            "name": "Carbonara",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Spaghetti", "Speck", "Eier", "Parmesan", "Pfeffer"],
+            "price": 11.49,
+            "img": "./assets/img/Pasta4.jpeg",
+            },
+            {
+            "name": "Lasagne",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Lasagneblätter", "Rinderhackfleisch", "Tomatensauce", "Béchamelsauce", "Mozzarella", "Parmesan"],
+            "price": 12.99,
+            "img": "./assets/img/Pasta5.jpeg",
+            },
+        ],
+    },
+
+    "Kolumbianisch": {
+        "banner": 
+            {
+                "banner-img": "./assets/img/.png",
+            },
+        "info": [
+            {
+            "name": "",
+            "listLabel": "Zutaten:",
+            "ingredients": [""],
+            "price": 9.99,
+            "img": "./assets/img/.jpeg",
+            },
+            {
+            "name": "",
+            "listLabel": "Zutaten:",
+            "ingredients": [""],
+            "price": 8.99,
+            "img": "./assets/img/.jpeg", 
+            },
+            {
+            "name": "",
+            "listLabel": "Zutaten:",
+            "ingredients": [""],
+            "price": 7.99,
+            "img": "./assets/img/.jpeg",
+            },
+            {
+            "name": "",
+            "listLabel": "Zutaten:",
+            "ingredients": [""],
+            "price": 10.49,
+            "img": "./assets/img/.jpeg",
+            },
+            {
+            "name": "",
+            "listLabel": "Zutaten:",
+            "ingredients": [""],
+            "price": 11.49,
+            "img": "./assets/img/.jpeg",
+            }
+        ],
+    },
+
+    "Salat": {
+        "banner": 
+            {
+                "banner-img": "./assets/img/Salad.png",
+            },
+        "info": [
+            {
+            "name": "Caesar Salat",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Römersalat", "Croutons", "Parmesan", "Caesar-Dressing", "Hähnchenbruststreifen"],
+            "price": 9.99,
+            "img": "./assets/img/Salat1.jpeg",
+            },
+            {
+            "name": "Griechischer Salat",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Gurken", "Tomaten", "rote Zwiebeln", "Feta-Käse", "Oliven", "Oregano", "Olivenöl"],
+            "price": 8.99,
+            "img": "./assets/img/Salat2.jpeg", 
+            },
+            {
+            "name": "Caprese Salat",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Tomatenscheiben", "Mozzarella", "frisches Basilikum", "Balsamico-Reduktion", "Olivenöl"],
+            "price": 7.99,
+            "img": "./assets/img/Salat3.jpeg",
+            },
+            {
+            "name": "Nicoise Salat",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Thunfisch", "grüne Bohnen", "Kartoffeln", "Eier", "Oliven", "Tomaten", "Vinaigrette"],
+            "price": 10.49,
+            "img": "./assets/img/Salat4.jpeg",
+            },
+            {
+            "name": "Mediterraner Halloumi-Salat",
+            "listLabel": "Zutaten:",
+            "ingredients": ["Blattsalate", "gegrillter Halloumi", "Kirschtomaten", "Gurken", "rote Zwiebeln", "Oliven", "geröstete Pinienkerne", "Honig-Senf-Dressing"],
+            "price": 11.49,
+            "img": "./assets/img/Salat5.jpeg",
+            }
+        ],
+    },
+
+    "Getränk": {
+        "banner": 
+            {
+                "banner-img": "./assets/img/Burger.png",
+            },
+        "info": [
+            {
+            "name": "Coca-Cola",
+            "listLabel": "Sorte:",
+            "ingredients": ["Classic", "Zero", "Light"],
+            "price": 2.49,
+            "img": "./assets/img/Getränk1.jpeg",
+            },
+            {
+            "name": "Mineralwasser",
+            "listLabel": "Sorte:",
+            "ingredients": ["mit Kohlensäure", "ohne Kohlensäure"],
+            "price": 1.99,
+            "img": "./assets/img/Getränk2.jpeg",
+            },
+            {
+            "name": "Saft",
+            "listLabel": "Sorte:",
+            "ingredients": ["Orange", "Apfel", "Banane", "Erdbeere", "Mango", "Maracuja", "Beerenmix"],
+            "price": 2.99,
+            "img": "./assets/img/Getränk3.jpeg",
+            },
+            {
+            "name": "Bier",
+            "listLabel": "Sorte:",
+            "ingredients": ["Pils", "Weizen", "Radler", "Alkoholfrei"],
+            "price": 3.49,
+            "img": "./assets/img/Getränk4.jpeg",
+            },
+            {
+            "name": "Kaffee",
+            "listLabel": "Sorte:",
+            "ingredients": ["Espresso", "Cappuccino", "Latte Macchiato", "Americano"],
+            "price": 2.99,
+            "img": "./assets/img/Getränk5.jpeg",
+            }
+        ]
+    }
+}
