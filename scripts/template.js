e4f7d1898ea08de76afa12012a50f3537b04c78b
MarcelN911 <marceln911@icloud.com>
2026-01-29 14:39:07 -0500

    add script for category selection

---
+++ scripts/template.js
@@ -0,0 +1,18 @@
+function renderCategorys() {
+    const categorysContent = document.getElementById("category-selection");
+    categorysContent.innerHTML = "";
+
+    const categorys = Object.keys(menu);
+
+    for (let index = 0; index < categorys.length; index++) {
+        const category = categorys[index];
+        const categoryButton = document.createElement("button");
+        categoryButton.textContent = category;
+        categoryButton.classList.add("category-btn");
+        categorysContent.appendChild(categoryButton);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    renderCategorys();
+});
